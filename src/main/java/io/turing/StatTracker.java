7d1453ddc4b882694e08b0dfb17b7343ed807833
earl-stephens <stephensearl@hotmail.com>
2023-02-18 22:26:36 -0700

    Start test for CSV parsing

--- src/main/java/io/turing/StatTracker.java
+++ src/main/java/io/turing/StatTracker.java
@@ -1,7 +1,22 @@
 package io.turing;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class StatTracker {
+	private String file1;
+	private String file2;
+	private String file3;
+	public List<HashMap<String, String>> game_teams = new ArrayList<>();
+	public List<HashMap<String, String>> games = new ArrayList<>();
+	public List<HashMap<String, String>> teams = new ArrayList<>();
+	
 	public StatTracker(String file1Location, String file2Location, String file3Location) {
-		
+		file1 = file1Location;
+		file2 = file2Location;
+		file3 = file3Location;
 	}
+	
+	
 }
--- src/test/java/io/turing/StatTrackerTest.java
+++ src/test/java/io/turing/StatTrackerTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class StatTrackerTest {
-
+	
 	@Test
 	void testForStatTrackerObject() {
 		StatTracker statTracker = new StatTracker("File1", "File2", "File3");
@@ -13,4 +13,13 @@ class StatTrackerTest {
 		assertNotNull(statTracker);
 	}
 
+	@Test
+	void testThatParserCreatesAnArrayOfHashes() {
+		String file1 = "game_teams.csv";
+		String file2 = "games.csv";
+		String file3 = "teams.csv";
+		StatTracker statTracker = new StatTracker(file1, file2, file3);
+		
+		assertEquals(32, statTracker.teams.size());
+	}
 }
