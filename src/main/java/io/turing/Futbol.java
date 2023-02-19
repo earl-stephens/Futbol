41e42ea42beec9f7c38b9839e8359b6bf0e8e13d
earl-stephens <stephensearl@hotmail.com>
2023-02-18 21:06:37 -0700

    Set up main method

--- src/main/java/io/turing/Futbol.java
+++ src/main/java/io/turing/Futbol.java
@@ -4,7 +4,12 @@ public class Futbol {
 
 	public static void main(String[] args) {
 
-
+		String game_teams = "game_teams.csv";
+		String games = "games.csv";
+		String teams = "teams.csv";
+		
+		StatTracker statTracker =  new StatTracker(game_teams, games, teams);
+		
 	}
 
 }
