4f20a0284568ca831eff713266a7dd219af1e69e
earl-stephens <stephensearl@hotmail.com>
2023-02-18 23:31:03 -0700

    Test and code done for parsing csv files

--- src/main/java/io/turing/StatTracker.java
+++ src/main/java/io/turing/StatTracker.java
@@ -1,22 +1,38 @@
 package io.turing;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
 public class StatTracker {
-	private String file1;
-	private String file2;
-	private String file3;
-	public List<HashMap<String, String>> game_teams = new ArrayList<>();
-	public List<HashMap<String, String>> games = new ArrayList<>();
-	public List<HashMap<String, String>> teams = new ArrayList<>();
-	
+
+	// Use List as the outer collection because the size of the csv file can change
+	// Use String[] as the inner collection since the number of headers in the
+	// csv file is unlikely to change
+	public List<String[]> game_teams;
+	public List<String[]> games;
+	public List<String[]> teams;
+
 	public StatTracker(String file1Location, String file2Location, String file3Location) {
-		file1 = file1Location;
-		file2 = file2Location;
-		file3 = file3Location;
+		game_teams = parseFile(file1Location, game_teams);
+		games = parseFile(file2Location, games);
+		teams = parseFile(file3Location, teams);
 	}
-	
-	
+
+	public List<String[]> parseFile(String fileLocation, List<String[]> listName) {
+		try {
+			FileReader fileReader = new FileReader(fileLocation);
+			CSVReader csvReader = new CSVReaderBuilder(fileReader).withSkipLines(1).build();
+			listName = csvReader.readAll();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return listName;
+	}
 }
--- src/test/java/io/turing/StatTrackerTest.java
+++ src/test/java/io/turing/StatTrackerTest.java
@@ -1,6 +1,7 @@
 package io.turing;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,18 +9,23 @@ class StatTrackerTest {
 	
 	@Test
 	void testForStatTrackerObject() {
-		StatTracker statTracker = new StatTracker("File1", "File2", "File3");
+		String file1 = "/Users/earltstephens/eclipse-workspace/futbol/game_teams.csv";
+		String file2 = "/Users/earltstephens/eclipse-workspace/futbol/games.csv";
+		String file3 = "/Users/earltstephens/eclipse-workspace/futbol/teams.csv";
+		StatTracker statTracker = new StatTracker(file1, file2, file3);
 		
 		assertNotNull(statTracker);
 	}
 
 	@Test
 	void testThatParserCreatesAnArrayOfHashes() {
-		String file1 = "game_teams.csv";
-		String file2 = "games.csv";
-		String file3 = "teams.csv";
+		String file1 = "/Users/earltstephens/eclipse-workspace/futbol/game_teams.csv";
+		String file2 = "/Users/earltstephens/eclipse-workspace/futbol/games.csv";
+		String file3 = "/Users/earltstephens/eclipse-workspace/futbol/teams.csv";
 		StatTracker statTracker = new StatTracker(file1, file2, file3);
 		
 		assertEquals(32, statTracker.teams.size());
+		assertEquals(14882, statTracker.game_teams.size());
+		assertEquals(7441, statTracker.games.size());
 	}
 }
