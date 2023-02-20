f25a47344e1a992af451d094bbd3cf8c373762b7
earl-stephens <stephensearl@hotmail.com>
2023-02-19 21:39:04 -0700

    Add test for highest total score

--- src/test/java/io/turing/StatTrackerTest.java
+++ src/test/java/io/turing/StatTrackerTest.java
@@ -28,4 +28,14 @@ class StatTrackerTest {
 		assertEquals(14882, statTracker.game_teams.size());
 		assertEquals(7441, statTracker.games.size());
 	}
+	
+	@Test
+	void testHighestTotalScore() {
+		String file1 = "/Users/earltstephens/eclipse-workspace/futbol/game_teams.csv";
+		String file2 = "/Users/earltstephens/eclipse-workspace/futbol/games_test.csv";
+		String file3 = "/Users/earltstephens/eclipse-workspace/futbol/teams.csv";
+		StatTracker statTracker = new StatTracker(file1, file2, file3);
+		
+		assertEquals(9, statTracker.highestTotalScore())
+;	}
 }
